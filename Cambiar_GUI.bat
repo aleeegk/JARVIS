a7701c0c9879f1bb301e6073a64879106f572ffd
aleeegl <alejandroferreira2023@gmail.com>
2026-09-04 12:29:25 +0200

    chore: remover demos y lanzadores experimentales del repositorio remoto

--- Cambiar_GUI.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-title JARVIS - Cambiar Interfaz Grafica
-color 0B
-python "%~dp0cambiar_gui.py" %*
--- Iniciar_JARVIS_GUI.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title JARVIS - Iniciar GUI Activa
-color 0E
-python "%~dp0cambiar_gui.py" --launch
-if %ERRORLEVEL% NEQ 0 (
-    echo.
-    echo [ERROR] No se pudo iniciar la interfaz grafica activa.
-    pause
-)
